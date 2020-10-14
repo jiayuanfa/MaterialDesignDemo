552f052a98677d8d4d42320867423eee060493bf
jiayuanfa <jiayuanfa@yeah.net>
2020-10-15 00:53:23 +0800

    卡片式布局、侧滑

---
+++ app/src/main/java/com/example/materialdesigndemo/Fruit.kt
@@ -0,0 +1,4 @@
+package com.example.materialdesigndemo
+
+class Fruit(val name: String, val imageId: Int) {
+}
--- app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
+++ app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
@@ -5,13 +5,62 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val fruit = mutableListOf(Fruit("Apple", R.drawable.apple),
+        Fruit("Banana", R.drawable.banana),
+        Fruit("Orange", R.drawable.orange),
+        Fruit("Watermelon", R.drawable.watermelon),
+        Fruit("Pear", R.drawable.pear),
+        Fruit("Grape", R.drawable.grape),
+        Fruit("Pineapple", R.drawable.pineapple),
+        Fruit("Strawberry", R.drawable.strawberry),
+        Fruit("Cherry", R.drawable.cherry),
+        Fruit("Mango", R.drawable.mango))
+
+    val fruitList = ArrayList<Fruit>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        navView.setCheckedItem(R.id.navCall)
+        navView.setNavigationItemSelectedListener {
+            drawerLayout.closeDrawers()
+            true
+        }
+        fab.setOnClickListener {view ->
+            Snackbar.make(view, "Data deleted", Snackbar.LENGTH_SHORT)
+                .setAction("Undo") {
+                    Toast.makeText(this, "FAB Click", Toast.LENGTH_SHORT).show()
+                }
+                .show()
+        }
+
+        initRv()
+    }
+
+    private fun initRv() {
+        fruitList.clear()
+        repeat(50) {
+            val index = (0 until fruit.size).random()
+            fruitList.add(fruit[index])
+        }
+        val layoutManager = GridLayoutManager(this, 2)
+        mRv.layoutManager = layoutManager
+        val adapter = FruitAdapter(this, fruitList)
+        mRv.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -29,6 +78,9 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.setting -> {
                 Toast.makeText(this, "You click settings", Toast.LENGTH_SHORT).show()
+            }
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
             }
         }
         return true
